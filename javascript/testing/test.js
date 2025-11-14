49ddee7948570a719676ca7144ea67e9ebc8258a
Xattle <ntgrgr@gmail.com>
2025-11-13 20:40:41 -0600

    begin javascript section

---
+++ javascript/testing/test.js
@@ -0,0 +1,38 @@
+console.log("Hi there!");
+
+let firstname = "John";
+let surname = "Doe";
+
+console.log(firstname);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+// pi = 10;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1+1));
+
+// Exercises
+
+console.log(23 + 97);
+console.log(42 + 230 + 3 + 30 + 49 + 438);
+console.log((4+6+9)/77);
+
+let a = 10;
+console.log(a);
+a = 13;
+console.log(a);
+let b = 7*a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
